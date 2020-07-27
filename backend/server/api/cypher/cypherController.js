8d593641a8f288b71ebca8b3b5dc24667d6362a3
hwha <hwha@bitnine.net>
2020-07-28 08:52:16 +0900

    [#AGV3-38]수정: Cypher 질의 응답 포맷 변경

--- backend/server/api/cypher/cypherController.js
+++ backend/server/api/cypher/cypherController.js
@@ -4,33 +4,60 @@ let ag = require('agensgraph');
 let router = express.Router();
 
 router.post('/', function (req, res, next) {
-    let cmd = req.body.cmd;
-    if(!req.session.client) {
+    if (!req.session.client) {
         res.status(500);
     } else {
-        let clientInfo = req.session.client;
-        let connInfo = {
-            user: clientInfo.user,
-            password: clientInfo.password,
-            database: clientInfo.database,
-            host: clientInfo.host,
-            port: clientInfo.port
+        let cmd = req.body.cmd;
 
-        }
-
-        const client = new ag.Client(connInfo);
+        const client = new ag.Client(req.session.client);
         client.connect();
-        client.query(`set graph_path=${clientInfo.graph}`);
+        client.query(`set graph_path=${req.session.client.graph}`);
         client.query(cmd, (err, resultSet) => {
-            if(err) {
-                throw err;
-            } else {
-                console.log(resultSet.rows);
-                res.status(200).json(resultSet.rows).end();
-            }
-        })
+            let rows = resultSet.rows, columns = resultSet.fields.map((d) => d.name);
 
+            let convertedRows = rows.map((row) => {
+                let convetedObject = {};
+                for (let k in row) {
+                    if (row[k].hasOwnProperty('start')) {
+                        convetedObject[k] = convertEdge(row[k]);
+                    } else if (row[k].hasOwnProperty('id')) {
+                        convetedObject[k] = convertVertex(row[k]);
+                    } else {
+                        convetedObject[k] = row[k];
+                    }
+                }
+                return convetedObject;
+            });
+
+            result = convertedRows;
+            res.status(200).json({
+                message: 'OK',
+                data: {
+                    rows: convertedRows,
+                    columns: columns,
+                },
+            });
+            client.end();
+        });
     }
 });
+
+function convertEdge({ label, id, start, end, props }) {
+    return {
+        label: label,
+        id: `${id.oid}.${id.id}`,
+        start: `${start.oid}.${start.id}`,
+        end: `${end.oid}.${end.id}`,
+        properties: props,
+    };
+}
+
+function convertVertex({ label, id, props }) {
+    return {
+        label: label,
+        id: `${id.oid}.${id.id}`,
+        properties: props,
+    };
+}
 
 module.exports = router;
