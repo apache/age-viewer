90abdc15df89ec54abcf87819ce16d8ae9fcc225
hdseo <hd.seo@bitnine.net>
2020-08-04 17:14:27 +0900

    [#AGBV3-59] 추가: Navigator 퍼블리싱

--- frontend/src/components/sidebar/presentations/Sidebar.jsx
+++ frontend/src/components/sidebar/presentations/Sidebar.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import SidebarHome from './SidebarHome'
 import SidebarSetting from './SidebarSetting'
 
-
 const Sidebar = ({ activeMenuName, isActive, changeTheme }) => {
     return (        
-        <div id="sidebar" className={isActive ? " active " : ""}>
+        <div id="sidebar" className={isActive ? " active " : ""} style={{overflowY: 'scroll'}}>
                 <div className="tab-content">
                     <div className={"tab-pane fade" + (activeMenuName === "home" ? " active show " : "") } role="tabpanel" aria-labelledby="side-home-tab">
                         <SidebarHome />
