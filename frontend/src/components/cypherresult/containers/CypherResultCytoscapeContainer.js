deddfeab2b5f4cd66b29899c2262e2cfad437404
Karl Jeong <gsjeong@bitnine.net>
2020-08-07 08:41:41 +0900

    [#AGBV3-74] 추가 : Label별 색상 변경
    1. Label 클릭 시 해당 차트의 동일 Label의 색상을 변경
    
    해결 #AGBV3-74

--- frontend/src/components/cypherresult/containers/CypherResultCytoscapeContainer.js
+++ frontend/src/components/cypherresult/containers/CypherResultCytoscapeContainer.js
@@ -2,18 +2,18 @@ import { connect } from 'react-redux'
 import CypherResultCytoscape from '../presentations/CypherResultCytoscape'
 
 const labelColors = [
-    {color: '#664B00', borderColor: '#662500', labels : new Set([]), index : 0},  
-    {color: '#D1B2FF', borderColor: '#A566FF', labels : new Set([]), index : 1}, 
-    {color: '#FFC19E', borderColor: '#F29661', labels : new Set([]), index : 2}, 
-    {color: '#B5B2FF', borderColor: '#6B66FF', labels : new Set([]), index : 3}, 
-    {color: '#F15F5F', borderColor: '#CC3D3D', labels : new Set([]), index : 4}, 
-    {color: '#C4B73B', borderColor: '#998A00', labels : new Set([]), index : 5}, 
-    {color: '#9FC93C', borderColor: '#6B9900', labels : new Set([]), index : 6}, 
-    {color: '#FFD9EC', borderColor: '#FFB2D9', labels : new Set([]), index : 7}, 
-    {color: '#6799FF', borderColor: '#4374D9', labels : new Set([]), index : 8}, 
-    {color: '#FFBB00', borderColor: '#DB9700', labels : new Set([]), index : 9}, 
-    {color: '#FFB2D9', borderColor: '#F361A6', labels : new Set([]), index : 10}, 
-    {color: '#6B9900', borderColor: '#476600', labels : new Set([]), index : 11}
+    {color: '#604A0E', borderColor: '#423204', fontColor: '#FFF', labels : new Set([]), index : 0},  
+    {color: '#C990C0', borderColor: '#B261A5', fontColor: '#FFF', labels : new Set([]), index : 1}, 
+    {color: '#F79767', borderColor: '#F36924', fontColor: '#FFF', labels : new Set([]), index : 2}, 
+    {color: '#57C7E3', borderColor: '#23B3D7', fontColor: '#2A2C34', labels : new Set([]), index : 3}, 
+    {color: '#F16667', borderColor: '#EB2728', fontColor: '#FFF', labels : new Set([]), index : 4}, 
+    {color: '#D9C8AE', borderColor: '#C0A378', fontColor: '#2A2C34', labels : new Set([]), index : 5}, 
+    {color: '#8DCC93', borderColor: '#5DB665', fontColor: '#2A2C34', labels : new Set([]), index : 6}, 
+    {color: '#ECB5C9', borderColor: '#DA7298', fontColor: '#2A2C34', labels : new Set([]), index : 7}, 
+    {color: '#498EDA', borderColor: '#2870C2', fontColor: '#FFF', labels : new Set([]), index : 8}, 
+    {color: '#FFC454', borderColor: '#D7A013', fontColor: '#2A2C34', labels : new Set([]), index : 9}, 
+    {color: '#DA7194', borderColor: '#CC3C6C', fontColor: '#FFF', labels : new Set([]), index : 10}, 
+    {color: '#569480', borderColor: '#447666', fontColor: '#FFF', labels : new Set([]), index : 11}
   ]
 
 const mapStateToProps = (state, ownProps) => {
@@ -25,6 +25,7 @@ const mapStateToProps = (state, ownProps) => {
             if (labelColor.labels.has(labelName) ) {
                 selectedColor.push(labelColor.color)
                 selectedColor.push(labelColor.borderColor)
+                selectedColor.push(labelColor.fontColor)
                 return
             }
         })
@@ -34,6 +35,7 @@ const mapStateToProps = (state, ownProps) => {
             labelColors[randomIndex].labels.add(labelName)
             selectedColor.push(labelColors[randomIndex].color)
             selectedColor.push(labelColors[randomIndex].borderColor)
+            selectedColor.push(labelColors[randomIndex].fontColor)
         }
 
         return selectedColor;
@@ -50,21 +52,20 @@ const mapStateToProps = (state, ownProps) => {
                 for (const [alias, val] of Object.entries(row)) {
                     let labelName = val['label']
                     if (val['start'] && val['end']) {
-                        if (!edgeLegend.hasOwnProperty(labelName)) { edgeLegend[labelName] = getRandomColor(labelName) }
+                        if (!edgeLegend.hasOwnProperty(labelName)) { edgeLegend[labelName] = ['#8C8C8C', '#8C8C8C', '#2A2C34'] }
                         edges.push(
-                            { group: 'edges', data: { id: val.id, source: val.start, target: val.end, label: val.label, backgroundColor: edgeLegend[labelName][0], borderColor: edgeLegend[labelName][1], properties: val.properties }, alias: alias, classes: ['node'] }
+                            { group: 'edges', data: { id: val.id, source: val.start, target: val.end, label: val.label, backgroundColor: edgeLegend[labelName][0], borderColor: edgeLegend[labelName][1], fontColor: edgeLegend[labelName][2], properties: val.properties }, alias: alias, classes: ['node'] }
                         )
                     } else {
                         if (!nodeLegend.hasOwnProperty(labelName)) { nodeLegend[labelName] = getRandomColor(labelName) }
                         nodes.push(
-                            { group: 'nodes', data: { id: val.id, label: val.label, backgroundColor: nodeLegend[labelName][0], borderColor: nodeLegend[labelName][1], properties: val.properties }, alias: alias,  classes: ['node'] }
+                            { group: 'nodes', data: { id: val.id, label: val.label, backgroundColor: nodeLegend[labelName][0], borderColor: nodeLegend[labelName][1], fontColor: nodeLegend[labelName][2], properties: val.properties }, alias: alias,  classes: ['node'] }
                         )
                     }
                 }
             });
 
         }
-
         return { legend : {nodeLegend: nodeLegend, edgeLegend: edgeLegend}, elements : { nodes: nodes, edges: edges }}
 
     }
--- frontend/src/components/cypherresult/presentations/CypherResultCytoscapeLegend.jsx
+++ frontend/src/components/cypherresult/presentations/CypherResultCytoscapeLegend.jsx
@@ -1,50 +1,61 @@
-import React, {Component} from 'react';
-import {Badge} from 'react-bootstrap'
+import React, { Component } from 'react';
+import { Badge } from 'react-bootstrap'
 import uuid from 'react-uuid'
 
-class CypherResultCytoscapeLegend extends Component{
+class CypherResultCytoscapeLegend extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      nodeBadges: new Map(),
+      edgeBadges: new Map()
+    }
   }
 
-  nodeBadges = []
-  edgeBadges = []
 
-  componentDidMount(){
+
+  componentDidMount() {
   }
 
-  shouldComponentUpdate(){
+  shouldComponentUpdate() {
     return true;
   }
 
-  componentWillReceiveProps(nextProps){
-    if (Object.keys(this.props.legendData.nodeLegend).length === 0 && Object.keys(this.props.legendData.edgeLegend).length === 0) {
-        for (const [label, color] of Object.entries(nextProps.legendData.nodeLegend)) {
-          this.nodeBadges.push(<Badge className="nodeLabel px-3 py-2 mx-1 my-2" pill key={uuid()} onClick={() => nextProps.onLabelClick({type:'labels', data:{type:'node', backgroundColor : color[0], label : label}})} style={{ backgroundColor : color[0], fontSize : '0.9rem' }}>{label}</Badge>)
-        }
-      
-  
-        for (const [label, color] of Object.entries(nextProps.legendData.edgeLegend)) {
-          this.edgeBadges.push(<Badge className="edgeLabel px-3 py-2 mx-1 my-2" key={uuid()} onClick={() => nextProps.onLabelClick({type:'labels', data:{type:'edge', backgroundColor : color[0], label : label}})} style={{ backgroundColor : color[0], fontSize : '0.9rem' }}>{label}</Badge>)
-        }
-
+  componentWillReceiveProps(nextProps) {
+    for (const [label, color] of Object.entries(nextProps.legendData.nodeLegend)) {
+      this.state.nodeBadges.set(label, <Badge className="nodeLabel px-3 py-2 mx-1 my-2" pill key={uuid()} onClick={() => nextProps.onLabelClick({ type: 'labels', data: { type: 'node', backgroundColor: color[0], fontColor: color[2], label: label } })} style={{ backgroundColor: color[0], color: color[2] }}>{label}</Badge>)
     }
 
+
+    for (const [label, color] of Object.entries(nextProps.legendData.edgeLegend)) {
+      this.state.edgeBadges.set(label, <Badge className="edgeLabel px-3 py-2 mx-1 my-2" key={uuid()} onClick={() => nextProps.onLabelClick({ type: 'labels', data: { type: 'edge', backgroundColor: color[0], fontColor: color[2], label: label } })} style={{ backgroundColor: color[0], color: color[2] }}>{label}</Badge>)
+    }
+
+
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
   }
 
 
-  render(){
-    return <div className="legend-area" style={{ width: '100%'}}>
+  render() {
+
+    let nodeLedgend = []
+    let edgeLedgend = []
+    this.state.nodeBadges.forEach((value, key, mapObj) => {
+      return nodeLedgend.push(value)
+    })
+    this.state.edgeBadges.forEach((value, key, mapObj) => {
+      return edgeLedgend.push(value)
+    })
+
+    return <div className="legend-area" style={{ width: '100%' }}>
       <div className="nodeLegend">
-      {this.nodeBadges}
+        {nodeLedgend}
       </div>
       <div className="edgeLegend">
-      {this.edgeBadges}
+        {edgeLedgend}
       </div>
-      
+
     </div>
   }
 }
