fd73ef6d6c5014eb1fd2ec93aaaf23fedee0c9c9
Karl Jeong <gsjeong@bitnine.net>
2020-08-12 10:03:52 +0900

    [#AGBV3-79] BUGFIX : Frame Reload시 Label이 사라지지 않는 현상
    1. Label 색상 변경 후, Reload시 이전 색상이 유지되는 현상 수정
    
    2. Chrat Extend 후, Reload시 Label이 사라지지 않는 현상 수정
    
    해결 #AGBV3-79

--- frontend/src/components/cypherresult/containers/CypherResultCytoscapeContainer.js
+++ frontend/src/components/cypherresult/containers/CypherResultCytoscapeContainer.js
@@ -1,14 +1,13 @@
 import { connect } from 'react-redux'
 import CypherResultCytoscape from '../presentations/CypherResultCytoscape'
-import { generateCytoscapeElement, labelColors } from '../../../features/cypher/CypherUtil'
+import { generateCytoscapeElement, labelColors, getCurrentLabelColor } from '../../../features/cypher/CypherUtil'
 
 const mapStateToProps = (state, ownProps) => {
     const { refKey } = ownProps
 
     return {
         data: generateCytoscapeElement(state.cypher.queryResult[refKey]),
-        isActive: state.navigator.isActive,
-        labelColors: labelColors
+        isActive: state.navigator.isActive
     }
 }
 
