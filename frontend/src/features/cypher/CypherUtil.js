9f9642f3c2e9ba0328e05f6a5265c61e436ae114
KarlJeong <behappy0723@gmail.com>
2020-09-14 15:50:37 +0900

    [#AGBV3-154] Bugfix : 최초 Chart가 그려질 때, Edge의 Caption이 보이지 않는 현상

--- frontend/src/features/cypher/CypherUtil.js
+++ frontend/src/features/cypher/CypherUtil.js
@@ -17,18 +17,18 @@ export const nodeLabelColors = [
 ]
 
 export const edgeLabelColors = [
-    { color: '#604A0E', borderColor: '#423204', fontColor: '#FFF', edgeLabels: new Set([]), index: 0 },
-    { color: '#C990C0', borderColor: '#B261A5', fontColor: '#FFF', edgeLabels: new Set([]), index: 1 },
-    { color: '#F79767', borderColor: '#F36924', fontColor: '#FFF', edgeLabels: new Set([]), index: 2 },
+    { color: '#CCA63D', borderColor: '#997000', fontColor: '#2A2C34', edgeLabels: new Set([]), index: 0 },
+    { color: '#C990C0', borderColor: '#B261A5', fontColor: '#2A2C34', edgeLabels: new Set([]), index: 1 },
+    { color: '#F79767', borderColor: '#F36924', fontColor: '#2A2C34', edgeLabels: new Set([]), index: 2 },
     { color: '#57C7E3', borderColor: '#23B3D7', fontColor: '#2A2C34', edgeLabels: new Set([]), index: 3 },
-    { color: '#F16667', borderColor: '#EB2728', fontColor: '#FFF', edgeLabels: new Set([]), index: 4 },
+    { color: '#F16667', borderColor: '#EB2728', fontColor: '#2A2C34', edgeLabels: new Set([]), index: 4 },
     { color: '#D9C8AE', borderColor: '#C0A378', fontColor: '#2A2C34', edgeLabels: new Set([]), index: 5 },
     { color: '#8DCC93', borderColor: '#5DB665', fontColor: '#2A2C34', edgeLabels: new Set([]), index: 6 },
     { color: '#ECB5C9', borderColor: '#DA7298', fontColor: '#2A2C34', edgeLabels: new Set([]), index: 7 },
-    { color: '#498EDA', borderColor: '#2870C2', fontColor: '#FFF', edgeLabels: new Set([]), index: 8 },
+    { color: '#498EDA', borderColor: '#2870C2', fontColor: '#2A2C34', edgeLabels: new Set([]), index: 8 },
     { color: '#FFC454', borderColor: '#D7A013', fontColor: '#2A2C34', edgeLabels: new Set([]), index: 9 },
-    { color: '#DA7194', borderColor: '#CC3C6C', fontColor: '#FFF', edgeLabels: new Set([]), index: 10 },
-    { color: '#569480', borderColor: '#447666', fontColor: '#FFF', edgeLabels: new Set([]), index: 11 }
+    { color: '#DA7194', borderColor: '#CC3C6C', fontColor: '#2A2C34', edgeLabels: new Set([]), index: 10 },
+    { color: '#569480', borderColor: '#447666', fontColor: '#2A2C34', edgeLabels: new Set([]), index: 11 }
 ]
 
 
