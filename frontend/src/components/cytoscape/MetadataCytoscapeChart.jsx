8228abe8f9a3e6c9946cbbb03ad0b3157b8f4322
Alex Kwak <take-me-home@kakao.com>
2021-02-16 16:14:55 +0900

    refactoring on MetadataCytoscapeChart.jsx

--- frontend/src/components/cytoscape/MetadataCytoscapeChart.jsx
+++ frontend/src/components/cytoscape/MetadataCytoscapeChart.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import cytoscape from 'cytoscape';
 import COSEBilkent from 'cytoscape-cose-bilkent';
@@ -8,9 +8,9 @@ import klay from 'cytoscape-klay';
 import euler from 'cytoscape-euler';
 import avsdf from 'cytoscape-avsdf';
 import spread from 'cytoscape-spread';
+import CytoscapeComponent from 'react-cytoscapejs';
 import { defaultLayout } from './CytoscapeLayouts';
 import { stylesheet } from './CytoscapeStyleSheet';
-import conf from './CytoscapeConfig';
 
 cytoscape.use(COSEBilkent);
 cytoscape.use(cola);
@@ -20,98 +20,43 @@ cytoscape.use(euler);
 cytoscape.use(avsdf);
 cytoscape.use(spread);
 
-class CytoscapeComponent extends Component {
-  constructor(props) {
-    super(props);
+const CytoscapeFitPadding = 200;
 
-    this.state = {
-      cytoscapeElement: null,
-      cy: null,
-      handleUserAction: this.handleUserAction.bind(this),
-    };
-  }
+const MetadataCytoscapeChart = ({ elements }) => {
+  const [cytoscapeObject, setCytoscapeObject] = useState(null);
 
-  componentDidMount() {
-    const { cytoscapeElement } = this.state;
-
-    conf.container = cytoscapeElement;
-    conf.pan = { x: cytoscapeElement.offsetWidth / 3, y: 50 };
-    conf.style = stylesheet;
-    conf.layout = defaultLayout;
-
-    this.setState({
-      cy: cytoscape(conf),
+  const cyCallback = useCallback((newCytoscapeObject) => {
+    if (cytoscapeObject) return;
+    newCytoscapeObject.ready(() => {
+      newCytoscapeObject.fit(null, CytoscapeFitPadding);
+      newCytoscapeObject.center();
     });
-  }
+    newCytoscapeObject.on('resize', () => {
+      newCytoscapeObject.center();
+    });
+    setCytoscapeObject(newCytoscapeObject);
+  },
+  [cytoscapeObject]);
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.elements.nodes.length === 0) {
-      this.cy.add(nextProps.elements);
-      this.cy.layout(defaultLayout).run();
-
-      prevState.handleUserAction(nextProps, false);
+  useEffect(() => {
+    if (cytoscapeObject) {
+      cytoscapeObject.fit(null, CytoscapeFitPadding);
+      cytoscapeObject.center();
     }
-    return null;
-  }
+  }, [elements]);
 
-  shouldComponentUpdate() {
-    return false;
-  }
+  return (
+    <CytoscapeComponent
+      elements={CytoscapeComponent.normalizeElements(elements)}
+      stylesheet={stylesheet}
+      layout={defaultLayout}
+      cy={cyCallback}
+      className="chart-area metachart-area"
+    />
+  );
+};
 
-  componentWillUnmount() {
-    const { cy } = this.state;
-    cy.destroy();
-  }
-
-  handleUserAction(props, areNewElements) {
-    const { cy } = this.state;
-    const targetElements = areNewElements ? cy.elements('.new') : cy.elements();
-
-    targetElements.bind('mouseover', (e) => {
-      e.target.addClass('highlight');
-    });
-
-    targetElements.bind('mouseout', (e) => {
-      e.target.removeClass('highlight');
-    });
-
-    targetElements.bind('click', (e) => {
-      const ele = e.target;
-      if (ele.selected() && ele.isNode()) {
-        if (cy.nodes(':selected').size() === 1) {
-          ele.neighborhood().selectify().select().unselectify();
-        } else {
-          cy.nodes(':selected').filter(`[id != "${ele.id()}"]`).neighborhood().selectify()
-            .select()
-            .unselectify();
-        }
-      } else {
-        cy.elements(':selected').unselect().selectify();
-      }
-    });
-
-    cy.bind('click', (e) => {
-      if (e.target === cy) {
-        cy.elements(':selected').unselect().selectify();
-      }
-    });
-  }
-
-  render() {
-    return (
-      <div
-        className="chart-area metachart-area"
-        ref={(el) => {
-          this.setState({
-            cytoscapeElement: el,
-          });
-        }}
-      />
-    );
-  }
-}
-
-CytoscapeComponent.propTypes = {
+MetadataCytoscapeChart.propTypes = {
   elements: PropTypes.shape({
     nodes: PropTypes.arrayOf(PropTypes.shape({
       // eslint-disable-next-line react/forbid-prop-types
@@ -124,4 +69,4 @@ CytoscapeComponent.propTypes = {
   }).isRequired,
 };
 
-export default CytoscapeComponent;
+export default MetadataCytoscapeChart;
