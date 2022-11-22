d799b900e9234d5ce9ff5488ceec45f4d83c3b09
Marodin Shahbaz <shahbazm@oregonstate.edu>
2022-11-22 15:04:26 -0800

    modal features and file upload

--- frontend/src/components/initializer/presentation/GraphInitializer.jsx
+++ frontend/src/components/initializer/presentation/GraphInitializer.jsx
@@ -1,34 +1,83 @@
+import React, { useState, useRef } from 'react';
 import {
-  Form, Modal, Row, Col,
+  /* Form, */ Modal, Row, Col, Button, ListGroup,
 } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import style from './GraphInit.scss';
 
 const InitGraphModal = ({ show, setShow }) => {
-  const [data] = useState(0);
+  const [nodeFiles, setNodeFiles] = useState([]);
+  const [edgeFiles, setEdgeFiles] = useState([]);
+  const edgeInputRef = useRef();
+  const nodeInputRef = useRef();
 
+  const handleSelectNodeFiles = (e) => {
+    console.log(e.target.files);
+    setNodeFiles([...nodeFiles, ...e.target.files]);
+  };
+
+  const handleSelectEdgeFiles = (e) => {
+    setEdgeFiles([...edgeFiles, ...e.target.files]);
+  };
+
+  const removeNodeFile = (e) => {
+    const index = e.target['data-index'];
+    nodeFiles.splice(index, 1);
+    setNodeFiles([...nodeFiles]);
+  };
+
+  const removeEdgeFile = (e) => {
+    const index = e.target['data-index'];
+    edgeFiles.splice(index, 1);
+    setEdgeFiles([...edgeFiles]);
+  };
   return (
-    <Modal show={show} onHide={() => setShow(!show)}>
-      <Row>
-        <Col>
-          <Form>
-            <Form.Group>
-              <Form.Label>
-                Test
-              </Form.Label>
-              <Form.Text>
-                Fill
-              </Form.Text>
-              <Form.Text>
-                {data}
-              </Form.Text>
-            </Form.Group>
-          </Form>
-        </Col>
+    <div className={style.ModalContainer}>
+      <Modal className={style.Modal} show={show} onHide={() => setShow(!show)}>
+        <Modal.Header closeButton>
+          Upload Files
+        </Modal.Header>
+        <Modal.Body>
+          <Row>
+            <Col>
+              <Button onClick={() => nodeInputRef.current.click()}>
+                Upload Nodes
+                <input type="file" ref={nodeInputRef} onChange={handleSelectNodeFiles} multiple hidden />
+              </Button>
+              <Button onClick={() => edgeInputRef.current.click()}>
+                Upload Edges
+                <input type="file" ref={edgeInputRef} onChange={handleSelectEdgeFiles} multiple hidden />
+              </Button>
+            </Col>
+          </Row>
+          <ListGroup>
+            {
+              nodeFiles.map((file, i) => (
+                <ListGroup.Item>
+                  {file.name}
+                  <Button size="small" data-index={i} onClick={removeNodeFile}> x </Button>
+                </ListGroup.Item>
+              ))
+          }
+            {
+              edgeFiles.map((file, i) => (
+                <ListGroup.Item>
+                  {file.name}
+                  <Button size="small" data-index={i} onClick={removeEdgeFile}> x </Button>
+                </ListGroup.Item>
+              ))
+          }
+          </ListGroup>
 
-      </Row>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button>
+            Done
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
 
-    </Modal>
   );
 };
 
