112c86131c23bc80b80636c757a27083960f3d19
marodins <67288224+marodins@users.noreply.github.com>
2022-12-16 10:02:30 -0800

    Graph initializer beta feature (#80)
    * implement modal
    
    * modal features and file upload
    
    * update adding and removing files
    
    * graph file upload complete
    
    * extract data from files
    
    * update style
    
    * initialize files and type names
    
    * setup parsing and implement graph creator
    
    * implement parsing of csv
    
    * implement backend file retrieval
    
    * send files to backend; implement file parsing in backend and remove parsing from frontend
    
    * implement query builder and data parser
    
    * update parsing methods and properties; implement serializing util; include test
    
    * allow existing graph to be dropped
    
    * update backend error handling and frontend success
    
    * update error handling
    
    * implement transactions and update graph metadata
    
    * update file uploads
    
    * fix undefined label
    
    * only accept csv input
    
    * set drop to proper bool val
    
    * implement clear input
    
    * node id value as string when creating edges

--- backend/src/services/databaseService.js
+++ backend/src/services/databaseService.js
@@ -201,7 +201,10 @@ class DatabaseService {
                 cur = 'edges';
             }
             else{
-                meta[cur].push(element);
+                if(meta[cur]){
+                    meta[cur].push(element);
+                }
+                
             }
 
         });
